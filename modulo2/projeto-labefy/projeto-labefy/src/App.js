7f8930f3e2b18e165152e07d1bac6bfb005468b4
rafaelgarcianunes13@gmail.com <rafaelgarcianunes13@gmail.com>
2022-06-26 16:16:07 -0300

    bug sendo resolvido

--- modulo2/projeto-labefy/projeto-labefy/src/App.js
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import AlterarPlays from './components/alterarPlaylist'
+import Header from './components/cabecalho'
+import Playists from './components/criarPlaylist'
 
 export default class App extends React.Component {
+  state = {
+    paginas: 'Playlists'
+  }
+
   render() {
+    EscolherPaginas = () => {
+      switch (this.state.paginas) {
+        case 'Playlists':
+          return <Playists />
+        case 'Alterar':
+          return <AlterarPlays />
+        default:
+          return 'Erro'
+      }
+    }
     return (
       <div>
-        <h1>Projeto Labefy</h1>
+        <Header />
+        <Playists />
+        <AlterarPlays />
       </div>
     )
   }
---
+++ modulo2/projeto-labefy/projeto-labefy/src/components/alterarPlaylist/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AltPlay = styled.div`
+  background-color: blue;
+`
+AlterarPlays = () => {
+  return (
+    <AltPlay>
+      <h1>Alterar Playlists</h1>
+    </AltPlay>
+  )
+}
+
+export default AlterarPlays
