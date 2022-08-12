1abbc2b742b2170b43dc8bbfc4214ccb129b4a80
rafaelgarcianunes13@gmail.com <rafaelgarcianunes13@gmail.com>
2022-08-11 21:10:03 -0300

    aprofundamentos typescript

---
+++ modulo4/aprofundamento-typescript/src/exercicio4.ts
@@ -0,0 +1,28 @@
+//B-) usaria o comando 'tsc' e logo em seguida o comando 'node build/esxercicio4.js'
+//C-)Não
+//D-)Podemos digitar o comando tsv seguido dos nomes dos arquivos seoarados por espaços
+
+
+type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
