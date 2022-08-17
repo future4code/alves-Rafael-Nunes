4871a5dc8ef7199011034b805bdbd9f88e3f36af
rafaelgarcianunes13@gmail.com <rafaelgarcianunes13@gmail.com>
2022-08-16 22:56:43 -0300

    exercicio 3 check

--- modulo4/aprofundamento-express/src/index.ts
+++ modulo4/aprofundamento-express/src/index.ts
@@ -18,9 +18,33 @@ app.get('/ping', (req:Request, res:Response) => {
 
 //Exercicio 2
 
-type tasks ={
+type Task ={
     userId:string | number,
     id:string | number,
     title:string ,
     completed:boolean
 }
+
+//Exercicio 3
+
+const tasks:Task[] =
+[
+  {
+    userId: 1,
+    id: 5,
+    title: 'levar o doguinho para passear',
+    completed: true
+  },
+  {
+    userId: 2,
+    id: 3,
+    title: 'fazer um array para servir de banco de dados',
+    completed: false
+  },
+  {
+    userId: 3,
+    id: 2,
+    title: 'fazer o exercicio 1',
+    completed: true
+  }
+] 
