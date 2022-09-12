9866d8e18b1976ab29296bdca05bd495c149e2fd
rafaelgarcianunes13@gmail.com <rafaelgarcianunes13@gmail.com>
2022-09-12 20:11:44 -0300

    configurando atividade

---
+++ modulo5/introducao-autenticacao/src/index.ts
@@ -0,0 +1,30 @@
+import knex from "knex";
+import dotenv from "dotenv";
+import express from "express";
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+const connection = knex({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
+  },
+});
+
+const app = express();
+
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
