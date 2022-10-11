4602686a99a11ca77d8d6a3b524551494e297a96
rafaelgarcianunes13@gmail.com <rafaelgarcianunes13@gmail.com>
2022-10-10 21:31:51 -0300

    ainda to quebrando a cabeça com react

--- modulo6/case-brainn/brain/src/App.js
+++ modulo6/case-brainn/brain/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { getLotery } from './services/callAPI'
+import { Lotery } from './services/callAPI'
 import HomePage from './components/pages/homePage/HomePage'
 import axios from 'axios'
+import BASE_URL from './services/baseURL'
 
 function App() {
-  // getLotery()
-
   return (
     <div>
       <HomePage />
--- modulo6/case-brainn/brain/src/components/pages/homePage/HomePage.js
+++ modulo6/case-brainn/brain/src/components/pages/homePage/HomePage.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import { StyleHomePage } from './styleHomePage'
+// import { useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+// import BASE_URL from '../../../services/baseURL'
+// import { getLotery } from '../../../services/callAPI'
+import {
+  NavBar,
+  StyleHomePage,
+  Img,
+  SelectCamp,
+  NameLotery,
+  Contest,
+  NumberContest
+} from './styleHomePage'
+import { imageMega } from '../../assets/image'
+import { Lotery } from '../../../services/callAPI'
 
 export default function HomePage() {
   return (
     <StyleHomePage>
-      <h1>HomePage</h1>
+      <NavBar>
+        <SelectCamp>
+          <option>{Lotery}</option>
+        </SelectCamp>
+        <NameLotery>MEGA-SENA</NameLotery>
+        <Img>{imageMega}</Img>
+        <Contest>concurso</Contest>
+        <NumberContest>4531 – 07/04/2020</NumberContest>
+      </NavBar>
+
+      <div>
+        <h1>local Numbers</h1>
+      </div>
     </StyleHomePage>
   )
 }
