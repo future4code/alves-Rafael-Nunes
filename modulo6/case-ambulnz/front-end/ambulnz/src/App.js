275561191760c9eef410206f7b44b2e7c68c5e67
rafaelgarcianunes13@gmail.com <rafaelgarcianunes13@gmail.com>
2022-10-20 21:57:16 -0300

    bug no carrinho

---
+++ modulo6/case-ambulnz/front-end/ambulnz/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import Cart from './Pages/Cart/Cart'
+import HomePage from './Pages/Home/HomePage'
+
+export const AppMain = styled.div`
+  display: flex;
+  background-color: blue;
+`
+
+function App() {
+  const [cart, setCart] = useState([])
+
+  const addToCart = addPizza => {
+    const foundIndex = cart.indexOf(
+      pizzaInCart => pizzaInCart.name === addPizza.name
+    )
+
+    if (foundIndex >= 0) {
+      const newCart = [...cart]
+      newCart[foundIndex].quantity += 1
+
+      setCart(newCart)
+    } else {
+      const newCart = [...cart]
+      const newPizza = {
+        name: addPizza.name,
+        price: addPizza.price,
+        quantity: 1
+      }
+
+      newCart.push(newPizza)
+      setCart(newCart)
+    }
+  }
+
+  return (
+    <AppMain>
+      <HomePage addToCart={addToCart} />
+      <Cart cart={cart} />
+    </AppMain>
+  )
+}
+
+export default App
---
+++ modulo6/case-ambulnz/front-end/ambulnz/src/Pages/Home/HomePage.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const DivCard = styled.div`
+  display: flex;
+`
